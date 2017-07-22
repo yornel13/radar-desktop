496c7577cedf3d1e21b81b3da37aa4f54fe5883c
prigramer <prigramer30@gmail.com>
2017-07-21 20:46:08 -0400

    cardList_test

--- src/gui/workmanController.java
+++ src/gui/workmanController.java
@@ -8,18 +8,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import model.User;
 import service.RadarService;
 
-import java.awt.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -27,22 +27,15 @@ import java.util.ResourceBundle;
 public class workmanController implements Initializable {
 
     @FXML
-    JFXListView<Label> listView;
+    JFXListView<HBox> listView;
 
     @FXML
     Pane cardPane;
 
-    @FXML
-    Label img;
 
-    @FXML
-    Label name;
-
-    @FXML
-    Label dni;
 
     List<User> users;
-    ObservableList<Label> data;
+    ObservableList<HBox> data;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -64,12 +57,21 @@ public class workmanController implements Initializable {
         data = FXCollections.observableArrayList();
         for (User user: users) {
 
+            HBox hbox = new HBox();
+            HBox imgHbox = new HBox();
+            VBox labelsVbox = new VBox();
+            Label imgLabel  = new Label();
+            imgLabel.setGraphic(new ImageView(new Image(new FileInputStream("src/gui/policeman32.png"))));
+            Label nameLabel = new Label("\t  "+user.getLastname()+" "+user.getName()+"\n");
+            Label dniLabel  = new Label("\t"+user.getDni());
+            dniLabel.setFont( new Font(null, 14));
+            dniLabel.setTextFill(Color.valueOf("#aaaaaa"));
+            imgHbox.getChildren().add(imgLabel);
+            labelsVbox.getChildren().addAll(nameLabel, dniLabel);
+            hbox.getChildren().addAll(imgHbox, labelsVbox);
 
-            Label dniLabel = new Label(user.getDni());
-            dniLabel.setFont(new Font("Arial", 15));
-            Label nameLabel = new Label(user.getLastname()+" "+user.getName()+"\n");
-            nameLabel.setGraphic(new ImageView(new Image(new FileInputStream("src/gui/policeman32.png"))));
-            data.addAll(nameLabel);
+            data.addAll(hbox);
+
 
 
         }
